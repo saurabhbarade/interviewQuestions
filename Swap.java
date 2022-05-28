bb18b682276bfe4366bae28e3503766c58fb261e
Saurabh Barade <baradesaurabh@gmail.com>
2022-05-28 19:50:04 +0530

    added

---
+++ Swap.java
@@ -0,0 +1,11 @@
+class Swap{
+	public static void main(String args[]){
+		int a,b;
+		a=20; b=14;
+		System.out.println("before swapping"+a+b);
+		b= a+b;
+		a=b-a;
+		b=b-a;
+		System.out.println("after swapping"+a+b);
+	}
+}
